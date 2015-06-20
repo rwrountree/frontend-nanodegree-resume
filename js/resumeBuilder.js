f7f6e608e18b0c234a39f2fb27a7ce8158dc845f
Rusty Rountree <rustonrountree@gmail.com>
2015-06-20 13:33:54 -0500

    Change header name and role to use my info

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,7 +1,21 @@
-$("#main").append("Rusty Rountree");
+//$("#main").append("Rusty Rountree");
 
 var speed_of_light = 299792458;   // meters per second
 var meter = 100;                  // one meter is 100 centimeters
 var nanosecond = 1.0/1000000000;  // one billionth of a second
 
 console.log(speed_of_light * (meter * nanosecond));
+
+//var awesomeThoughts = "I am Rusty and I am AWESOME!";
+//console.log(awesomeThoughts);
+//var funThoughts = awesomeThoughts.replace("AWESOME!", "FUN!");
+//$("#main").append(funThoughts);
+
+var my_name = "Rusty Rountree";
+var formattedName = HTMLheaderName.replace("%data%", my_name);
+$("#header").append(formattedName);
+
+var my_role = "Front End Ninja";
+var formattedRole = HTMLheaderRole.replace("%data%", my_role);
+$("#header").append(formattedRole);
+
