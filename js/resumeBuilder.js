320f861819dc4c6fa916b3455a6a9f24bdab3cad
Rusty Rountree <rustonrountree@gmail.com>
2015-06-20 13:03:37 -0500

    Add speed of ligt conversion

---
+++ js/resumeBuilder.js
@@ -0,0 +1,7 @@
+$("#main").append("Rusty Rountree");
+
+var speed_of_light = 299792458;   // meters per second
+var meter = 100;                  // one meter is 100 centimeters
+var nanosecond = 1.0/1000000000;  // one billionth of a second
+
+console.log(speed_of_light * (meter * nanosecond));
